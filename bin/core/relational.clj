7b21922a9abdaac18eccb39fc435f49ab419cdb4
Markus Bader <markus.bader@mni.thm.de>
2014-06-04 16:11:09 +0200

    bin files deleted

--- bin/core/relational.clj
+++
@@ -1,7 +0,0 @@
-(ns core.relational
-  (:require [clojure.repl :refer :all]))
-
-; entry point for core.relational; loads other classes
-
-(load "relation")
-(load "operators")
--- bin/core/relational_test.clj
+++
@@ -1,7 +0,0 @@
-(ns core.relational-test
-  (:use clojure.test
-        core.relational))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

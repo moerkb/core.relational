65ac0ac23f5d0506319b6e742cc5cbc5652982b1
Markus Bader <markus.bader@mni.thm.de>
2014-06-24 14:27:49 +0200

    Interface: relation now looks like a set of hash maps

--- src/core/relation.clj
+++ src/core/relation.clj
@@ -3,6 +3,34 @@
 
 (defrecord Relation [head body])
 
+(defn new-relation
+  "Give value tuples as a set of hash maps and it creates the relation"
+  [tuple-set]
+  (let [head (vec (keys (first tuple-set)))]
+    (Relation. 
+      head
+      (set (map (fn [tuple]
+                  (vec (map (fn [attr]
+                              (get tuple attr))
+                            head))) 
+                tuple-set)))))
+
+(defn rel-to-hash-map
+  "Returns a relation as a set of hash maps."
+  [rel]
+  (set (map (fn [tuple]
+              (apply merge (map (fn [attr val]
+                                  {attr val})
+                                (:head rel)
+                                tuple)))
+            (:body rel))))
+
+(defmethod print-method Relation 
+  [rel writer]
+  (.write writer (str "Relation" \newline
+                      "Header: " (:head rel) \newline 
+                      "Body:   " (rel-to-hash-map rel))))
+
 (defn create-relation 
   "Defines a new (typeless) relation. Head is the structure in form
   [attribute1, attribute2, ...] and body of the form #{ [value1-1 value1-2 ...] 
--- test/core/relation_test.clj
+++ test/core/relation_test.clj
@@ -1,5 +1,13 @@
 (ns core.relational-test)
 
+(deftest new-relation-test
+  (is (= (create-relation '[id name] #{[1 "Arthur"] [2 "Betty"]})
+         (new-relation #{ {'id 1 'name "Arthur"} {'id 2 'name "Betty"} }))))
+
+(deftest rel-to-hash-map-test
+  (is (= #{ {'id 1 'name "Arthur"} {'id 2 'name "Betty"} })
+         (rel-to-hash-map (create-relation '[id name] #{[1 "Arthur"] [2 "Betty"]}))))
+
 (deftest relation-test
   (testing "Relation creation"
     (let [head '[id name]

6eb7fda46d3e389fbf334a225a21c6533abe5726
Markus Bader <markus.bader@mni.thm.de>
2014-09-30 17:43:23 +0200

    SAP example corrected

--- src/core/relational/examples/sap.clj
+++ src/core/relational/examples/sap.clj
@@ -42,126 +42,130 @@ stop
 
 ; SAP01
 ; select sno, status from s where city = 'Paris'
-(print-relation (project (restrict s '(= :scity "Paris"))
-                 [:sno :status]))
+(project (restrict s #(= (% :scity) "Paris"))
+ [:sno :status])
 
 ; SAP02
 ; select distinct pno from sp
-(print-relation (project sp [:pno]))
+(project sp [:pno])
 
 ; SAP03
 ; select * from s
-(print-relation s)
+s
 
 ; SAP04
 ; select sno from s where city = 'Paris' and status > 20
-(print-relation (project (restrict s '(and (= :scity "Paris")
-                                          (> :status 20))) 
-                                  [:sno]))
+(project (restrict s #(and (= (% :scity) "Paris")
+                           (> (% :status) 20))) 
+                  [:sno])
 
 ; SAP05
 ; select sno, status from s where city = 'Paris' order by status desc
-(print-relation (project (restrict s '(= :scity "Paris")) [:sno :status]))
+(order (project (restrict s #(= (% :scity) "Paris")) 
+                [:sno :status])
+       {:status :desc})
 
 ; SAP06
 ; select pno, pname, weight from p where weight between 16 and 19
-(print-relation (project (restrict p '(and (>= :weight 16)
-                                          (<= :weight 19))) 
-                        [:pno :pname :weight]))
+(project (restrict p #(and (>= (% :weight) 16)
+                           (<= (% :weight) 19))) 
+        [:pno :pname :weight])
 
 ; SAP07
 ; select pno, pname, weight from p where weight in (12, 14, 19)
 ; or
 ; select pno, pname, weight from p where weight in (select weight from p
 ;                                                   where color = 'Red')
-(print-relation (restrict p '(or (= :weight 12) (= :weight 14) (= :weight 19))))
+(restrict p #(or (= (% :weight) 12) 
+                 (= (% :weight) 14) 
+                 (= (% :weight) 19)))
 
-(print-relation (project 
-                  (restrict p '(= "Red" :color)) 
-                  [:weight]))
-
-(print-relation (restrict p (fn [t] 
-                               (in? (project (restrict p '(= "Red" :color)) [:weight]) 
-                                          {:weight (:weight t)}))))
-(print-relation (intersect p (restrict p '(= "Red" :color))))
-(filter #(in? (restrict p '(= "Red" :color)) %)
-        (seq p))
+(project (restrict p (fn [t] (in? (project (restrict p #(= "Red" (% :color))) [:weight]) {:weight (:weight t)}) ))
+  [:pno :pname :weight])
 
 ; SAP08
 ; a) select * from s cross join p where s.city = p.city
-(print-relation (restrict (join s p) 
-                         '(= :scity :pcity)))
+(restrict (join s p) 
+         #(= (% :scity) (% :pcity)))
 
 ; b) select * from s join sp using (sno) join p using (pno) 
 ;        where s.city = p.city
 
-(print-relation (restrict (join p (join s sp))
-                         '(= :scity :pcity)))
+(restrict (join p (join s sp))
+         #(= (% :scity) (% :pcity)))
 
 ; SAP09
 ; select distinct s.city as 'delivering city', p.city as 'bearing city'
 ;   from s join sp using (sno) join p using (pno)
-; natural join cannot be used here!
+; (natural join cannot be used here!) => :city renamed to :scity and :pcity
 
-(print-relation (project (join s (join p sp))
-                       {:delivering-city :scity , :bearing-city :pcity}))
+(project (join s (join p sp))
+         {:delivering-city :scity , :bearing-city :pcity})
 
 ; SAP10
 ; select s1.sno, s2.sno from s as s1 cross join s as s2
 ;   where s1.city = s2.city and s1.sno < s2.sno
-(print-relation (project (restrict (join (rename* s #"(.+)" "s1-$1") 
-                                        (rename* s #"(.+)" "s2-$1"))
-                                  '(and (= :s1-scity :s2-scity) 
-                                        (< (Integer/parseInt (apply str (rest :s1-sno)))   
-                                           (Integer/parseInt (apply str (rest :s2-sno))))))
-                        [:s1-sno :s2-sno])) 
+
+#_(project (restrict (join (rename* s #"(.+)" "s1-$1") 
+                         (rename* s #"(.+)" "s2-$1"))
+                   '(and (= :s1-scity :s2-scity) 
+                         (< (Integer/parseInt (apply str (rest :s1-sno)))   
+                            (Integer/parseInt (apply str (rest :s2-sno))))))
+         [:s1-sno :s2-sno]) 
+
+(project (restrict (join (rename* s #"(.+)" "s1-$1") (rename* s #"(.+)" "s2-$1"))
+                   #(and (= (:s1-city %) (:s2-city %))
+                         (neg? (compare (:s1-sno %)  (:s2-sno %)))))
+         [:s1-sno :s2-sno])
 
 ; SAP11
 ; a) select sname from s natural join sp where sp.pno = 'P2'
-(print-relation (project (restrict (join s sp) 
-                                   (fn [t] (= (:pno t) "P2"))
-                                  '(= :pno "P2")) 
-                        [:sname]))
+(project (restrict (join s sp)
+                   #(= (% :pno) "P2")) 
+         [:sname])
 
 ; b) select sname from s where sno in (select sno from sp where pno = 'P2')
-(print-relation (project (restrict s
-                                   (fn [t] (in? :sno))
-                                  '(in? (project (restrict sp '(= :pno "P2")) [:sno])
-                                        :sno))
-                        [:sname]))
+
+(project (restrict s (fn [t] (in? (project (restrict sp #(= "P2" (:pno %))) [:sno])
+                                  {:sno (:sno t)})))
+  [:sname])
 
 ; SAP12
 ; select distinct sname from s join sp join p
 ;   where p.color = 'Red'
-(print-relation (project (restrict (join s (join sp p))
-                                  '(= :color "Red"))
-                        [:sname]))
+(project (restrict (join s (join sp p))
+                   #(= (% :color) "Red"))
+         [:sname])
 
 ; SAP13
 ; select count(sno) as Quantity from s
 (count s)
 ; resp.
-(print-relation (newrel {:Quantity (count s)}))
+(newrel {:Quantity (count s)})
 ; more precise
-(print-relation (summarize s nil {:Quantity #(count %)}))
+(summarize s nil {:Quantity #(count %)})
 
 ; SAP14
 ; select count(distinct sno) as Quantity from sp
 (count (project sp [:sno]))
 
+(summarize sp nil {:Quantity #(count (:sno %))})
+
 ; SAP15
 ; select sum(qty) as "Number of part P2" from sp where pno = 'P2'
-(print-relation (summarize (restrict sp '(= :pno "P2")) nil {:Qty #(reduce + (:qty %))}))
+(summarize (restrict sp #(= (% :pno) "P2")) nil {:Qty #(reduce + (:qty %))})
 
 ; SAP16
 ; select pno, sum(qty) as Quantity from sp group by pno order by pno
-(print-relation (summarize sp [:pno] {:quantity (fn [r] (reduce + (:qty r)))}))
+(order (summarize sp [:pno] {:quantity (fn [r] (reduce + (:qty r)))})
+  {:pno :asc})
 
 ; SAP17
 ; select pno from sp group by pno having count(*) > 1 order by pno
-(print-relation (project (restrict (summarize sp [:pno] {:cnt #(count (:sno %))})
-                                  '(> :cnt 1))
-                        [:pno]))
+(order (project (restrict (summarize sp [:pno] {:cnt #(count (:sno %))})
+                          #(> (% :cnt) 1))
+               [:pno])
+  {:pno :asc})
 
 
 ; === RELATION VARIABLES ===

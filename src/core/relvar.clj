9092d780ba90125619b9d64d08d5fb7b1f639218
Markus Bader <markus.bader@mni.thm.de>
2014-10-21 13:31:27 +0200

    Relvars with Refs

--- src/core/relvar.clj
+++ src/core/relvar.clj
@@ -5,17 +5,18 @@
   a collection of predicates, the relvar always has to satisfy when its value 
   is changed. Each such predicate takes the relation value as its only argument."
   ([relation]
-    (atom relation :meta {:constraints nil}))
+    (ref relation :meta {:constraints nil}))
   ([relation constraints]
-    (atom relation :meta {:constraints constraints})))
+    (ref relation :meta {:constraints constraints})))
 
 (defn assign!
   "Assigns a new relation value to a relation variable, but only if all
   constraints are satisfied. Otherwise, an IllegalArgumentException is 
   thrown and the relvar remains unchanged."
   [relvar new-relation]
-  (if (every? true? (map (fn [p] (p new-relation)) 
-                      (:constraints (meta relvar))))
-    (reset! relvar new-relation)
-    (throw (IllegalArgumentException. 
-             "The new relation value does not satisfy all constraints."))))
+  (dosync 
+    (if (every? true? (map (fn [p] (p new-relation)) 
+                       (:constraints (meta relvar))))
+     (ref-set relvar new-relation)
+     (throw (IllegalArgumentException. 
+              "The new relation value does not satisfy all constraints.")))))
--- test/core/relvar_test.clj
+++ test/core/relvar_test.clj
@@ -6,10 +6,22 @@
     (is (= r @(relvar r [(fn [r] (< (:id r) 21))])))))
 
 (deftest assign-test
-  (let [rvar (relvar (newrel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"}})
-               [(fn [r] (every? #(< % 21) (:id r)))
-                (fn [r] (apply distinct? (map :id (seq r))))])]
-    (is (= (newrel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"} {:id 3 :name "Carl"}})
-          (assign! rvar (union @rvar (newrel {:id 3 :name "Carl"})))))
-    (is (thrown? IllegalArgumentException (assign! rvar (union @rvar (newrel {:id 1  :name "Dora"})))))
-    (is (thrown? IllegalArgumentException (assign! rvar (union @rvar (newrel {:id 42 :name "Dora"})))))))
+  (testing "Assignment of relation with independent constraints." 
+    (let [rvar (relvar (newrel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"}})
+                [(fn [r] (every? #(< % 21) (:id r)))
+                 (fn [r] (apply distinct? (map :id (seq r))))])]
+     (is (= (newrel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"} {:id 3 :name "Carl"}})
+           (assign! rvar (union @rvar (newrel {:id 3 :name "Carl"})))))
+     (is (thrown? IllegalArgumentException (assign! rvar (union @rvar (newrel {:id 1  :name "Dora"})))))
+     (is (thrown? IllegalArgumentException (assign! rvar (union @rvar (newrel {:id 42 :name "Dora"})))))))
+  
+  (testing "Assignment of relation with constraints depending on other relations."
+    (let [rel (newrel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"}})
+          rvar1 (relvar (newrel #{{:pid 1 :phone "123456789"}
+                                  {:pid 2 :phone "987654321"}
+                                  {:pid 3 :phone "112233445"}}))
+          rvar2 (relvar rel
+                        [(fn [r] (every? #(in? (project @rvar1 [:pid]) {:pid %}) (:id r)))])]
+      (is (= (union rel (newrel {:id 3 :name "Carl"}))
+             (assign! rvar2 (union @rvar2 (newrel {:id 3 :name "Carl"})))))
+      (is (thrown? IllegalArgumentException (assign! rvar2 (union @rvar2 (newrel {:id 4 :name "Dori"}))))))))

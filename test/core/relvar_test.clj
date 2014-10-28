ea2e24a3748e557a43e55cf5729d2fc4220e3190
Markus Bader <markus.bader@mni.thm.de>
2014-10-28 15:16:40 +0100

    Relvar test made more meaningful

--- test/core/relvar_test.clj
+++ test/core/relvar_test.clj
@@ -16,12 +16,10 @@
      (is (thrown? IllegalArgumentException (assign! rvar (union @rvar (rel {:id 42 :name "Dora"})))))))
   
   (testing "Assignment of relation with constraints depending on other relations."
-    (let [r (rel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"}})
-          rvar1 (relvar (rel #{{:pid 1 :phone "123456789"}
-                               {:pid 2 :phone "987654321"}
-                               {:pid 3 :phone "112233445"}}))
+    (let [r (rel #{{:pid 1 :phone "123456789"}}) 
+          rvar1 (relvar (rel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"}}))
           rvar2 (relvar r
-                        [(fn [r] (every? #(in? (project @rvar1 [:pid]) {:pid %}) (:id r)))])]
-      (is (= (union r (rel {:id 3 :name "Carl"}))
-             (assign! rvar2 (union @rvar2 (rel {:id 3 :name "Carl"})))))
-      (is (thrown? IllegalArgumentException (assign! rvar2 (union @rvar2 (rel {:id 4 :name "Dori"}))))))))
+                  [(fn [r] (every? #(in? (project @rvar1 [:id]) {:id %}) (:pid r)))])]
+      (is (= (union r (rel {:pid 2 :phone "987654321"}))
+             (assign! rvar2 (union @rvar2 (rel {:pid 2 :phone "987654321"})))))
+      (is (thrown? IllegalArgumentException (assign! rvar2 (union @rvar2 (rel {:pid 4 :phone "32132"}))))))))

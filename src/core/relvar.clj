b7c79b0ed8714511ad182a912fd03385d1264ae3
Markus Bader <markus.bader@mni.thm.de>
2014-11-03 15:16:37 +0100

    involved relvars extracted from constraints

--- src/core/relvar.clj
+++ src/core/relvar.clj
@@ -1,13 +1,31 @@
 (ns core.relational)
 
+(defn involved-relvars
+  "Takes a collection of constraints and returns a set of all relvars that 
+  appear in them."
+  [constraints]
+  (if (empty? constraints)
+    #{}
+    (loop [symlist (flatten (map #(:body (meta %)) constraints))
+           res #{}]
+      (if (empty? symlist)
+        res
+        (if (and 
+              (symbol? (first symlist))
+              (= (first symlist) 'clojure.core/deref))
+          (recur (drop 2 symlist)
+                 (conj res (second symlist)))
+          (recur (rest symlist) 
+                 res))))))
+
 (defn relvar 
   "Creates a relation variable from the given relation (value). Constraints is 
   a collection of predicates, the relvar always has to satisfy when its value 
   is changed. Each such predicate takes the relation value as its only argument."
   ([relation]
-    (ref relation :meta {:constraints nil}))
+    (ref relation :meta {:constraints nil, :involved-relvars []}))
   ([relation constraints]
-    (ref relation :meta {:constraints constraints})))
+    (ref relation :meta {:constraints constraints, :involved-relvars (involved-relvars constraints)})))
 
 (defn assign!
   "Assigns a new relation value to a relation variable, but only if all
--- test/core/relvar_test.clj
+++ test/core/relvar_test.clj
@@ -1,17 +1,27 @@
 (ns core.relational-test)
 
+(deftest involved-relvars-test
+  (let [empty-var (relvar dum)
+        evar2 (relvar dee)
+        constrs [(relfn [t] (> (:status t) 5))
+                 (relfn [t] (in? @empty-var t))
+                 (relfn [t] (> (:status t) (max (:status @evar2))))]]
+    (is (= #{} (involved-relvars [])))
+    (is (= #{'empty-var 'evar2} (involved-relvars constrs)))))
+
 (deftest relvar-test
   (let [r (rel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"}})]
     (is (= r @(relvar r)))
-    (is (= r @(relvar r [(fn [r] (< (:id r) 21))])))))
+    (is (= r @(relvar r [(relfn [r] (< (:id r) 21))])))))
 
 (deftest assign-test
   (testing "Assignment of relation with independent constraints." 
     (let [rvar (relvar (rel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"}})
-                [(fn [r] (every? #(< % 21) (:id r)))
-                 (fn [r] (apply distinct? (map :id (seq r))))])]
+                [(relfn [r] (every? #(< % 21) (:id r)))
+                 (relfn [r] (apply distinct? (map :id (seq r))))])]
      (is (= (rel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"} {:id 3 :name "Carl"}})
            (assign! rvar (union @rvar (rel {:id 3 :name "Carl"})))))
+     (is (empty? (:involved-relvars (meta rvar))))
      (is (thrown? IllegalArgumentException (assign! rvar (union @rvar (rel {:id 1  :name "Dora"})))))
      (is (thrown? IllegalArgumentException (assign! rvar (union @rvar (rel {:id 42 :name "Dora"})))))))
   
@@ -19,7 +29,8 @@
     (let [r (rel #{{:pid 1 :phone "123456789"}}) 
           rvar1 (relvar (rel #{{:id 1 :name "Arthur"} {:id 2 :name "Betty"}}))
           rvar2 (relvar r
-                  [(fn [r] (every? #(in? (project @rvar1 [:id]) {:id %}) (:pid r)))])]
+                  [(relfn [r] (every? #(in? (project @rvar1 [:id]) {:id %}) (:pid r)))])]
       (is (= (union r (rel {:pid 2 :phone "987654321"}))
              (assign! rvar2 (union @rvar2 (rel {:pid 2 :phone "987654321"})))))
+      (is (= #{'rvar1} (:involved-relvars (meta rvar2))))
       (is (thrown? IllegalArgumentException (assign! rvar2 (union @rvar2 (rel {:pid 4 :phone "32132"}))))))))
